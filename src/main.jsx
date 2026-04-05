5ebd6ebe0037064b1346aea579da77136b57ff6c
Simeon Rediger <simeonrediger@gmail.com>
2026-04-05 17:51:31 -0400

    Add and apply Prettier config and ignore list

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import "./styles/reset.css";
-import "./styles/colors.css";
+import './styles/reset.css';
+import './styles/colors.css';
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App.jsx";
+import App from './App.jsx';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
