183b592c3470748fb98e6b5742f2b8b727751b36
Simeon Rediger <simeonrediger@gmail.com>
2026-04-06 15:54:08 -0400

    Set width and aspect ratio on image
    Prevents layout shifting due to image loading.

--- src/features/cards/Card.jsx
+++ src/features/cards/Card.jsx
@@ -4,6 +4,8 @@ import { formatMonsterName } from './card-utils.js';
 
 import CardInfoButton from './CardInfoButton.jsx';
 
+const monsterImageSize = 280;
+
 export default function Card({
   id,
   name,
@@ -18,7 +20,7 @@ export default function Card({
     <div className="card" data-card-id={id}>
       <h2 className="card-title">{name}</h2>
       <div className="card-image-and-info-button">
-        <img src={image}></img>
+        <img className="card-image" src={image} width={monsterImageSize}></img>
         <CardInfoButton
           dataRole={infoIconDataRole}
           className="card-info-button"
