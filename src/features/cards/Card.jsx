d485c0f9b5ab1d617ad95a99c9d8901f65b7b0a7
Simeon Rediger <simeonrediger@gmail.com>
2026-04-07 12:39:53 -0400

    Fix <img> to be self-closing tag

--- src/features/cards/Card.jsx
+++ src/features/cards/Card.jsx
@@ -20,7 +20,7 @@ export default function Card({
     <div className="card" data-card-id={id}>
       <h2 className="card-title">{name}</h2>
       <div className="card-image-and-info-button">
-        <img className="card-image" src={image} width={monsterImageSize}></img>
+        <img className="card-image" src={image} width={monsterImageSize} />
         <CardInfoButton
           dataRole={infoIconDataRole}
           className="card-info-button"
