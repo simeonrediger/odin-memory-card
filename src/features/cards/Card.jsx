84c29dc2e7e46529378891733a3395a0c35be3eb
Simeon Rediger <simeonrediger@gmail.com>
2026-04-08 15:10:11 -0400

    Style expanded card

--- src/features/cards/Card.jsx
+++ src/features/cards/Card.jsx
@@ -14,10 +14,16 @@ export default function Card({
   description,
   descriptionShown,
 }) {
+  let className = 'card';
+
+  if (descriptionShown) {
+    className += ' description-shown';
+  }
+
   name = formatMonsterName(name);
 
   return (
-    <div className="card" data-card-id={id}>
+    <div className={className} data-card-id={id}>
       <button className="card-hitbox">
         <h2 className="card-title">{name}</h2>
         <img className="card-image" src={image} width={monsterImageSize} />
@@ -29,7 +35,9 @@ export default function Card({
         width="28"
         height="28"
       />
-      {descriptionShown && <div>{description}</div>}
+      {descriptionShown && (
+        <div className="card-description">{description}</div>
+      )}
     </div>
   );
 }
--- src/features/cards/ToggleMoreButton.jsx
+++ src/features/cards/ToggleMoreButton.jsx
@@ -10,6 +10,7 @@ export default function ToggleMoreButton({
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
+        className="toggle-more-icon"
         width={width}
         height={height}
         fill="currentColor"
